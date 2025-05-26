a36524ee9de4f4427af2aa23d7b89986d2f1522f
Andri <abh8@hi.is>
2025-05-26 12:55:55 +0000

    laga

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Head() {
+  return (
+    <>
+      <link rel="preload" as="image" href="public\fiskibollur_portrait.svg" />
+      <link rel="preload" as="image" href="public\humarsupa_portrait.svg" />
+      <link rel="preload" as="image" href="public\fiskistangir_portrait.svg" />
+      <link rel="preload" as="image" href="public\plokkfiskur_portrait.svg" />
+    </>
+  );
+}
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ import styles from '@/styles/HeroSection.module.scss';
 
 const panels = [
   { src: '/fiskibollur_portrait.svg', alt: 'Fiskibollur' },
-  { src: '/humarsúpa_portrait.svg',   alt: 'Humarsúpa' },
+  { src: '/humarsupa_portrait.svg',   alt: 'Humarsúpa' },
   { src: '/fiskistangir_portrait.svg', alt: 'Fiskistangir' },
   { src: '/plokkfiskur_portrait.svg',  alt: 'Plokkfiskur' },
 ];
