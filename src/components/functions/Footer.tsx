ff9148f61c847eee34451b2d5ec9ff130f22513b
Andri <abh8@hi.is>
2025-06-12 10:09:52 +0000

    instagram logo

--- src/components/functions/Footer.tsx
+++ src/components/functions/Footer.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Instagram, Facebook } from 'lucide-react';
+import { Facebook } from 'lucide-react';
 import styles from '@/styles/Footer.module.scss';
 
 export default function Footer() {
@@ -31,6 +31,7 @@ export default function Footer() {
             quality={100}
           />
           <div className={styles.socialIcons}>
+            {/* Instagram with gradient glyph */}
             <Link
               href="https://www.instagram.com/grimurkokkur/"
               target="_blank"
@@ -38,10 +39,30 @@ export default function Footer() {
               aria-label="Instagram"
             >
               <div className={styles.icon}>
-                {/* Instagram brand color */}
-                <Instagram size={24} color="#E4405F" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  width="24"
+                  height="24"
+                >
+                  <defs>
+                    <linearGradient id="instaGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                      <stop offset="0%" stopColor="#f09433" />
+                      <stop offset="25%" stopColor="#e6683c" />
+                      <stop offset="50%" stopColor="#dc2743" />
+                      <stop offset="75%" stopColor="#cc2366" />
+                      <stop offset="100%" stopColor="#bc1888" />
+                    </linearGradient>
+                  </defs>
+                  <path
+                    fill="url(#instaGradient)"
+                    d="M349.33,69.33H162.67A93.34,93.34,0,0,0,69.33,162.67V349.33A93.34,93.34,0,0,0,162.67,442.67H349.33A93.34,93.34,0,0,0,442.67,349.33V162.67A93.34,93.34,0,0,0,349.33,69.33Zm61.33,279.99a61.42,61.42,0,0,1-61.33,61.33H162.67a61.42,61.42,0,0,1-61.33-61.33V162.67a61.42,61.42,0,0,1,61.33-61.33H349.33a61.42,61.42,0,0,1,61.33,61.33Zm-146.66-61.33A85.34,85.34,0,1,1,298,202.66,85.43,85.43,0,0,1,264,287.99Zm88-158.66a20,20,0,1,1-20,20A20,20,0,0,1,352,129.33Z"
+                  />
+                </svg>
               </div>
             </Link>
+
+            {/* Facebook with white glyph */}
             <Link
               href="https://www.facebook.com/Grimurkokkur"
               target="_blank"
@@ -49,8 +70,7 @@ export default function Footer() {
               aria-label="Facebook"
             >
               <div className={styles.icon}>
-                {/* Facebook brand color */}
-                <Facebook size={24} color="#1877F2" />
+                <Facebook size={24} color="#fff" />
               </div>
             </Link>
           </div>
